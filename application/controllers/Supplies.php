d84d1181efeb51e88fe89a49e97a09f7be6d83af
Andrew Lukonin <alukonin@my.bcit.ca>
2016-12-10 17:53:11 -0800

    Post/Put/Delete functionality and database table changes

--- application/controllers/Supplies.php
+++ application/controllers/Supplies.php
@@ -51,11 +51,20 @@ class Supplies extends Rest_controller
     {
         $key = $this->get('id');
         $record = array_merge(array('id' => $key), $_POST);
-        $this->Receiving->add($record);
-        $this->response(array('ok'), 200);
+        if ($this->validateFormInput($record, false))
+        {
+            $this->Receiving->add($record);
+            $this->response(array('New item created', $record), 200);
+        } else
+        {
+            $this->response(array('Invalid input'), 404);
+        }
+        //$this->Receiving->add($record);
+        //$this->response(array('ok'), 200);
     }
 
     // Handle an incoming POST - add a new menu item
+    /* REMOVED UNTIL NEEDED
     function item_post()
     {
         $key = $this->get('id');
@@ -63,21 +72,45 @@ class Supplies extends Rest_controller
         $this->Receiving->add($record);
         $this->response(array('ok'), 200);
     }
-
+    */
     // Handle an incoming PUT - update a menu item
     function index_put()
     {
         $key = $this->get('id');
         $record = array_merge(array('id' => $key), $this->_put_args);
-        $this->Receiving->update($record);
-        $this->response(array('ok'), 200);
+        if ($this->validateFormInput($record, true))
+        {
+            $this->Receiving->update($record);
+            $this->response(array('Updated record', $record), 200);
+        } else
+        {
+            $this->response(array('Failed to update record', $record), 404);
+        }
+        //$this->Receiving->update($record);
+        //$this->response(array('ok', $record), 200);
     }
 
-    // Handle an incoming DELETE - delete a menu item
+    // Handle an incoming DELETE
     function item_delete()
     {
         $key = $this->get('id');
-        $this->Receivings->delete($key);
-        $this->response(array('ok'), 200);
+        $this->Receiving->delete($key);
+        $this->response(array('ok', $key), 200);
+    }
+
+    function validateFormInput($input, $idRequired)
+    {
+        $required = ["name", "instock", "receiving", "measurement", "href"];
+        if ($idRequired)
+        {
+            array_push($required, "id");
+        }
+        $valid = true;
+        foreach ($required as $key)
+        {
+            if ( !(array_key_exists($key, $input)))
+                $valid = false;
+        }
+        return $valid;
     }
 }
